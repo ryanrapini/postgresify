1089f6f0ca9557eb29e94e9922e9b354a96f629d
Marti Markov <marti.markov@icloud.com>
2016-11-23 20:51:28 +0000

    Added support for native PgSQL arrays

--- src/Database/Eloquent/PostgresifyModel.php
+++ src/Database/Eloquent/PostgresifyModel.php
@@ -7,21 +7,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class PostgresifyModel extends Model
 {
+    use PostgresArray;
+
     protected $postgresifyCasts = [];
+
+    protected $postgresifyPrimitiveCasts = ['array'];
+
+    public function setAttribute($key, $value)
+    {
+        if (in_array($key, array_keys($this->postgresifyCasts))) {
+	    if (!is_object($value) && in_array($this->postgresifyCasts[$key]['type'], $this->postgresifyPrimitiveCasts) {
+		$value = self::mutateToPgArray($value);
+	    }
+	}
+        return parent::setAttribute($key, $value);
+    }
 
     public function getAttributeValue($key)
     {
         $value = parent::getAttributeValue($key);
 
-        if (in_array($key, array_keys($this->postgresifyCasts))) {
-            if (! is_null($value)) {
-                $postgresifyTypeCaster = new PostgresifyTypeCaster();
-                return $postgresifyTypeCaster->cast(
-                    $key,
-                    $value,
-                    $this->postgresifyCasts[$key]
-                );
-            }
+        if (!is_null($value) && in_array($key, array_keys($this->postgresifyCasts))) {
+	    if (in_array($this->postgresifyCasts[$key]['type'], $this->postgresifyPrimitiveCasts) {
+		return self::accessPgArray($value);
+	    }
+            $postgresifyTypeCaster = new PostgresifyTypeCaster();
+            return $postgresifyTypeCaster->cast(
+                $key,
+                $value,
+                $this->postgresifyCasts[$key]
+            );
         }
         
         return $value;
